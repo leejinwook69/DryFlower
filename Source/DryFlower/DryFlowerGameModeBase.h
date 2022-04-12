a922a186bb6a870ad18e9432424ff7fcf0613483
hwangsc777 <hwangcb777@naver.com>
2022-04-13 01:02:24 +0900

    First Commit

---
+++ Source/DryFlower/DryFlowerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DryFlowerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRYFLOWER_API ADryFlowerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
